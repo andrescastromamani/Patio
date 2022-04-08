761cbe474101accaffb54181892b6f8ba283fab5
Andrés Castro Mamani <castroandresrm@gmail.com>
2022-04-08 11:39:40 -0400

    fixed btn sidebar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Sidebar } from '../components/Sidebar';
 export const Home = () => {
     return (
         <div className="">
-            <button className="btn btn-dark" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar" aria-controls="offcanvasWithBackdrop"><i className="fa-solid fa-bars"></i></button>
+            <button className="btn btn-dark mt-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar" aria-controls="offcanvasWithBackdrop"><i className="fa-solid fa-bars"></i></button>
             <Sidebar />
             <h1 className="text-center">Inicio</h1>
         </div>
--- src/pages/Merchant.jsx
+++ src/pages/Merchant.jsx
@@ -5,7 +5,7 @@ export const Merchant = () => {
   return (
     <>
       <div className="shadow-sm p-3 bg-body rounded">
-        <button className="btn btn-dark" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar" aria-controls="offcanvasWithBackdrop"><i className="fa-solid fa-bars"></i></button>
+        <button className="btn btn-dark mt-4" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar" aria-controls="offcanvasWithBackdrop"><i className="fa-solid fa-bars"></i></button>
         <h2 className="text-center">Lista de Comerciantes</h2>
         <Sidebar />
       </div>
